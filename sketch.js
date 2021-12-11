0fa03b7f82ef353f9f2efb56c6e4366689e23c8f
kavyaadhikari <77389090+kavyaadhikari@users.noreply.github.com>
2021-12-11 19:43:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,10 +6,12 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, slingShot,bg;
+var gs = "onsling"
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+  
+    getTime();
 }
 
 function setup(){
@@ -36,14 +38,20 @@ function setup(){
     log4 = new Log(760,365,150, PI/7);
     log5 = new Log(870,365,150, -PI/7);
 
-    bird = new Bird(200,50);
+    bird = new Bird(200,250);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
+    slingshot = new SlingShot(bird.body,{x:200, y:250});
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    else{
+        background("lightblue");
+    }
+    
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -68,10 +76,39 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gs !== "launched"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gs = "launched"
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        bird.trajectory = [];
+        Matter.Body.setPosition(bird.body, {x: 200 , y: 250});
+        slingshot.attach(bird.body);
+        gs = "onsling"
+    }
+}
+
+async function getTime(){
+    var tlink = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var tjson = await tlink.json();
+    var dt = tjson.datetime;
+    var hr = dt.slice(11,13);
+    console.log(hr);
+
+    if(hr>=06 && hr<=18){
+        bg = "sprites/bg.png";
+    }
+    else{
+        bg = "sprites/night.jpg";
+    }
+    
+    backgroundImg = loadImage(bg);
 }
